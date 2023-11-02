494fe574f0192eb695ecde0c87040d78571184e6
Reinhardt de Beer <reinardt.de.beer@gmail.com>
2023-11-02 19:47:03 +0200

    Some last minute changes

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import joblib
 import numpy as np
+import pandas as pd
 
 # Load the model
 model = joblib.load("Spotify_SongDanceability_Predicter.pkl")
 
 # Load the model information
 model_info = joblib.load("model_info.pkl")
+
+print(model_info["features"])
 
 st.title("Predict a Songs Danceability")
 st.write("This is a ridge linear regression model")
@@ -20,7 +23,7 @@ for feature in model_info["features"]:
     elif feature == 'valence_%': 
         st.markdown("""---""")
         st.write("The musical positiveness:")
-        feature_values[feature] = st.number_input(f"Enter {feature}", min_value=1.0)
+        feature_values[feature] = st.number_input(f"Enter {feature}", min_value=1)
     elif feature == 'in_spotify_playlists': 
         st.markdown("""---""")
         st.write("In how many spotify playlists:")
@@ -28,18 +31,26 @@ for feature in model_info["features"]:
     elif feature == 'speechiness_%': 
         st.markdown("""---""")
         st.write("How much they sing in %")
-        feature_values[feature] = st.number_input(f"Enter {feature}", min_value=1.0)
+        feature_values[feature] = st.number_input(f"Enter {feature}", min_value=1)
     elif feature == 'bpm': 
         st.markdown("""---""")
         st.write("300bpm limit")
         feature_values[feature] = st.number_input(f"Enter {feature}", min_value=65, max_value=300)
     else:
-        feature_values[feature] = st.number_input(f"Enter {feature}", min_value=1.0)
+        feature_values[feature] = st.number_input(f"Enter {feature}", min_value=1)
         
         
 columns_to_log = ['bpm', 'in_spotify_playlists', 'speechiness_%', 'artist_count'] 
 
+# After collecting input values
 if st.button("Make Prediction"):
     input_features = [feature_values[feature] for feature in model_info["features"]]
-    prediction = model.predict([input_features])[0]
-    st.write(f"Predicted Stock Price: {prediction[0]}")
+    input_data = pd.DataFrame([input_features], columns=model_info["features"])
+    prediction = model.predict(input_data)[0]
+    st.write(f"Predicted Danceability: {prediction[0]} %")
+
+
+
+
+
+
